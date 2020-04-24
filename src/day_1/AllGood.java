1cd39201609d0cde8cf539661f1d47fdde45428d
ekaraoglu <64097926+ekaraoglu@users.noreply.github.com>
2020-04-24 14:31:08 -0600

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -5,5 +5,7 @@ public class AllGood {
     public static void main(String[] args) {
 
         System.out.println("I hope everything is good");
+        
+        // this line added from GitHub
     }
 }
